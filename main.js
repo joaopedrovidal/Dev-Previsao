6e1ee0c34dbd494f0c600bff6970a302d0a2f9a2
João Pedro Vidal <156215754+joaopedrovidal@users.noreply.github.com>
2024-08-13 18:44:04 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const key = "116d59cdef7ee452ede486b9740ddbc7";
+
+function DadosNaTela(dados){
+    document.querySelector(".city").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "ºC";
+    document.querySelector(".text-previsao").innerHTML = dados.weather[0].description;
+    document.querySelector(".text-umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function BuscarCidade(cidade) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`;
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
+    console.log(dados);
+    DadosNaTela(dados)
+}
+
+function ClickButton() {
+    const cidade = document.querySelector(".input-city").value;
+    console.log(cidade);
+    BuscarCidade(cidade); 
+}
